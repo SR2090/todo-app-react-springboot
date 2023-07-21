f3d4e67b633a7acb14fe0ef18757e5079b92dcfd
SR2090 <myworkemailid2557@gmail.com>
2023-07-21 15:27:40 +0530

    helloworldapi callout

--- src/components/todocomponents/WelcomeComponent.jsx
+++ src/components/todocomponents/WelcomeComponent.jsx
@@ -1,8 +1,17 @@
 import {Link, useParams } from 'react-router-dom'
-
+import axios from 'axios';
 export default function WelcomeComponent() {
     const {username} = useParams();
 
+    function callHelloWorldRestApi() {
+        // console.log("Call")
+        axios.get("http://localhost:8080/hello-world")
+        .then((response) => {console.log(response.data);})
+        .catch((error) => console.log(error))    
+        .finally(console.log("Finally block"));
+    }
+
+    
     return (
         <div className="welcome-screen">
         <h1 className="welcome-heading">Welcome, <span className="username">{username}</span>!</h1>
@@ -10,6 +19,9 @@ export default function WelcomeComponent() {
         <Link to={'/todos'}>
             <button className="start-button">Get Started</button>
         </Link>
+        <div className="container">
+            <button className="hello-button" onClick={callHelloWorldRestApi}>Hello World REST</button>
+        </div>
         </div>
     )
 }
